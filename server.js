7a5ba447176ac30a707112ec0e2c8d4f18ce6a28
丁小豪 <den2092@gmail.com>
2014-03-09 01:09:56 +0800

    P130
    TABLE : U/P

---
+++ server.js
@@ -0,0 +1,74 @@
+/**
+*
+* Module dependencies
+*
+**/
+
+var connect = require("connect")
+   ,users = require('./users')
+
+/**
+*
+* Create Server
+*
+**/
+
+var server = connect(
+	connect.logger('dev')
+   ,connect.bodyParser()
+   ,connect.cookieParser()
+   ,connect.session({ secret: 'my app secret' })
+   ,function (req, res, next) {//已登入且在目錄
+   		if ('/' == req.url && req.session.logged_in) {
+   			res.writeHead(200, {'Context-Type': 'text/html' });
+   			res.end(
+   					'Welcome back, <b>' + res.session.name + '</b>. '
+   					+ '<a href="/logout">Logout</a>'
+   			);
+   		} else {
+   			next();
+   		}
+   }
+   ,function (req, res, next) {//在目錄抓取使用者輸入(GET)的帳號密碼
+   		if ('/' == req.url && 'GET' == req.method) {
+   			res.writeHead(200, {'Context-Type': 'text/html' });
+   			res.end([
+   					'<form action="/login" method="POST">'
+   					,	'<fieldset>'
+   					,		'<legend> Please log in </legend>'
+   					,		'<p>User: <input type="text" name="user"></p>'
+   					,		'<p>Password: <input type="password" name="password"></p>'
+   					,		'<button>Submit</button>'
+   					,	'</fieldset>'
+   					,'</form>'
+   				].join(''));
+   		} else {
+   			next();
+   		}
+   }
+   ,function (req, res, next) {//跳到/login目錄且提交(POST)帳號密碼
+   		if ('/login' == req.url && 'POST' == req.method) {
+   			res.writeHead(200, {'Context-Type': 'text/html'})
+   			if (!users[req.body.user] || req.body.password != users[req.body.user].password) {
+   				res.end('Bad Username/Password');
+   			} else {
+   				req.session.logged_in = true;
+   				req.session.name = users[req.body.user].name;
+   				res.end('Authenticated!');
+   			}
+   		} else {
+   			next();
+   		}
+   }
+   ,function (req, res, next) {
+   	if ('/logout' == req.url) {
+   		req.session.logged_in = false;
+   		res.writeHead(200);
+   		res.end('Logged out!');
+   	} else {
+   		next();
+   	}
+   }
+);
+
+server.listen(3000);
